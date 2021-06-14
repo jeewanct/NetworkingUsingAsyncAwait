8edacf8749b40f2355115e09e5bcdd42078391a4
Jeevan <jeevan.tiwari@one97.net>
2021-06-15 01:53:46 +0530

    Initial Commit

---
+++ CuncurrencyBySwift/CuncurrencyBySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuncurrencyBySwiftApp.swift
+//  CuncurrencyBySwift
+//
+//  Created by jeevan tiwari on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CuncurrencyBySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
